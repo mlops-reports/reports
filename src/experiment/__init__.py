54252a2dc5b2266deaee5dbe9889bcdeb3d27b2b
gasci <g.asci@hotmail.com>
2023-08-18 16:43:39 +0200

    added query utils and example

--- src/experiment/__init__.py
+++ src/experiment/__init__.py
@@ -1,9 +1,13 @@
-import dotenv
+"""Report AI module"""
+from pathlib import Path
 
-__version__ = "0.0.1"
-
-__all__ = ["api"]
-# import environment variables
-dotenv.load_dotenv()
-
-
+NAME = "experiment"
+__build__ = {
+    line.strip().split()[-1]: line.strip().split()[0]
+    for line in (
+        (Path(__file__).parent / ".build").open()
+        if (Path(__file__).parent / ".build").exists()
+        else []
+    )
+}.get("HEAD", None)
+__all__ = ["api", "utils"]
